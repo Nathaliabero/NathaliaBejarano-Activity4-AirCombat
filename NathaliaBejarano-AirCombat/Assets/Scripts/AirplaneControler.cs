a797330fd36343bd1ed22a50804a9707e0817dab
Nathaliabero <nataliabejarano243@gmail.com>
2018-10-06 22:22:20 -0500

    Buenas

---
+++ NathaliaBejarano-AirCombat/Assets/Scripts/AirplaneControler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirplaneControler : MonoBehaviour {
+    public float Speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKey(KeyCode.UpArrow)) {
+            this.transform.Translate(Vector3.up*Speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            this.transform.Translate(Vector3.down*Speed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            this.transform.Translate(Vector3.left*Speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            this.transform.Translate(Vector3.right*Speed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            //creamos game object a partir de un prefab (planos de un edificio)
+            GameObject newMissile = GameObject.Instantiate(Resources.Load("Bullets/Missile") as GameObject);
+
+            GameObject shootPivot = GameObject.Find("Avión/ShootPivot");
+
+            newMissile.transform.SetParent(shootPivot.transform);
+
+            newMissile.transform.localPosition = Vector3.zero;
+
+            newMissile.transform.SetParent(null);
+        }
+
+
+
+    }
+}
---
+++ NathaliaBejarano-AirCombat/Assets/Scripts/MissileMotion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileMotion : MonoBehaviour {
+
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.Translate(Vector3.forward*Speed);
+        
+	}
+
+    void OnCollisionEnter(Collision collision) {
+        Debug.Log("Collision between two colliders");
+
+        GameObject.Destroy(this.gameObject);
+
+        GameObject.Destroy(collision.collider.gameObject);
+    }
+}
